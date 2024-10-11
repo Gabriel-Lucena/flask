de6e8be92f20e35b20864f5bf413e7e2eabf1e07
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2024-10-11 14:12:05 +0000

    Página inicial pronta

--- flask_app.py
+++ flask_app.py
@@ -3,17 +3,17 @@
 
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-#from flask_moment import Moment
-#from datetime import datetime
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
-#moment = Moment(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
-#@app.route('/user/<name>')
-#def name(name):
-#    return render_template('user.html', name=name)
+@app.route('/user/<name>')
+def name(name):
+    return render_template('user.html')
