01ba2fb609382473b327725189e5a96c8aef9f51
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2024-10-25 13:49:11 +0000

    Atividade da Semana 05 - Formulários Web feita
    Remoção das rotas de contexto da requisição e de usuário.

--- flask_app.py
+++ flask_app.py
@@ -30,17 +30,6 @@ def index():
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'))
 
-@app.route('/user/<name>/<prontuario>/<instituicao>')
-def user(name, prontuario, instituicao):
-    return render_template('user.html', name=name, prontuario=prontuario, instituicao=instituicao)
-
-@app.route('/contextorequisicao/<name>')
-def contextorequisicao(name):
-    navegador=request.headers.get('User-Agent')
-    ip_agent=request.remote_addr
-    base_url=request.host
-    return render_template('contextorequisicao.html', name=name, navegador=navegador, ip_agent=ip_agent, base_url=base_url)
-
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
