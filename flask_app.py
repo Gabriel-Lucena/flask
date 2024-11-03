36606a9290ec2adab1202ce4ac17d3d461391eda
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2024-11-03 23:28:25 +0000

    Começo da Inserção dos Campos no Template

--- flask_app.py
+++ flask_app.py
@@ -10,8 +10,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
-class NameForm(FlaskForm):
+class StudentForm(FlaskForm):
     name = StringField('Informe o seu nome', validators= [DataRequired()])
+    surname = StringField('Informe o seu sobrenome:', validators= [DataRequired()])
+    instituicao = StringField('Informe a sua Instituição de ensino:', validators= [DataRequired()])
+    disciplina = StringField('Informe a sua disciplina', validators= [DataRequired()])
     submit = SubmitField('Submit')
 
 app = Flask(__name__)
@@ -21,14 +24,17 @@ moment = Moment(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
+    form = StudentForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_surname = session.get('surname')
+        old_instituicao = session.get('instituicao')
+        old_disciplina = session.get('disciplina')
         if old_name is not None and old_name != form.name.data:
             flash('Você alterou o seu nome!')
         session['name'] = form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), surname=session.get('surname'), instituicao=session.get('instituicao'), disciplina=session.get('disciplina'))
 
 @app.errorhandler(404)
 def page_not_found(e):
