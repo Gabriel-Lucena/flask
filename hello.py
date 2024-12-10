203e93ffc3d42db0b127d3ab91f8c178e24febbd
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2024-12-10 01:56:15 +0000

    Aplicação Semana 11 Feita
    - Alteração de Favicon
    - Alteração de textos em inglês

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, BooleanField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -75,7 +75,9 @@ def send_simple_message(to, subject, newUser):
 
 
 class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[DataRequired()])
+    name = StringField('Qual é o seu nome?', validators=[DataRequired()])
+    status = BooleanField('Deseja enviar e-mail para flaskaulasweb@zohomail.com?')
+
     submit = SubmitField('Submit')
 
 
@@ -97,10 +99,14 @@ def internal_server_error(e):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
+
+    users = User.query.all()
+
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()
         if user is None:
-            user = User(username=form.name.data)
+            role = Role.query.filter_by(name="User").first()
+            user = User(username=form.name.data, role=role)
             db.session.add(user)
             db.session.commit()
             session['known'] = False
@@ -116,11 +122,15 @@ def index():
 
             if app.config['FLASKY_ADMIN']:
                 print('Enviando mensagem...', flush=True)
-                send_simple_message([app.config['FLASKY_ADMIN'], "flaskaulasweb@zohomail.com"], 'Novo usuário', form.name.data)
-                print('Mensagem enviada...', flush=True)
+                if form.status.data:
+                    send_simple_message([app.config['FLASKY_ADMIN'], "flaskaulasweb@zohomail.com"], 'Novo usuário', form.name.data)
+                    print('Mensagem enviada...', flush=True)
+                else:
+                    send_simple_message(app.config['FLASKY_ADMIN'], 'Novo usuário', form.name.data)
+                    print('Mensagem enviada...', flush=True)
         else:
             session['known'] = True
         session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'),
-                           known=session.get('known', False))
+                           known=session.get('known', False), users=users)
