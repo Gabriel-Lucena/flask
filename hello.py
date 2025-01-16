dad2af34e03261fc5ee200cab8939ff3b532c838
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2025-01-16 23:27:59 +0000

    Adição de Página Inicial

--- hello.py
+++ hello.py
@@ -2,11 +2,13 @@ import os
 from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -42,9 +44,24 @@ class User(db.Model):
         return '<User %r>' % self.username
 
 
+class Class(db.Model):
+    __tablename__ = 'classes'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True, index=True)
+    description = db.Column(db.String(64), unique=True, index=True)
+
+    def __repr__(self):
+        return '<Class %r>' % self.name
+
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+class ClassForm(FlaskForm):
+    name = StringField('Qual é o nome do curso?', validators=[DataRequired()])
+    description = StringField('Descrição (250 caracteres)')
+    submit = SubmitField('Cadastrar')
+
 
 
 @app.shell_context_processor
@@ -64,21 +81,37 @@ def internal_server_error(e):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
-    user_all = User.query.all();
-    print(user_all);
+    return render_template('index.html', current_time=datetime.utcnow());
+
+@app.route('/professores', methods=['GET', 'POST'])
+def professores():
+    return render_template('nao_disponivel.html', current_time=datetime.utcnow());
+
+@app.route('/disciplinas', methods=['GET', 'POST'])
+def disciplinas():
+    return render_template('nao_disponivel.html', current_time=datetime.utcnow());
+
+@app.route('/alunos', methods=['GET', 'POST'])
+def alunos():
+    return render_template('nao_disponivel.html', current_time=datetime.utcnow());
+
+
+@app.route('/cursos', methods=['GET', 'POST'])
+def cursos():
+    form = ClassForm()
+    class_all = Class.query.all();
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()                
-        if user is None:
-            user_role = Role.query.filter_by(name='User').first();
-            user = User(username=form.name.data, role=user_role);
-            db.session.add(user)
-            db.session.commit()
-            session['known'] = False
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'),
-                           known=session.get('known', False),
-                           user_all=user_all);
+
+        curso = Class(name=form.name.data, description=form.description.data);
+        db.session.add(curso)
+        db.session.commit()
+
+        return redirect(url_for('cursos'))
+    return render_template('cursos.html', form=form,
+                           class_all=class_all);
+
+@app.route('/ocorrencias', methods=['GET', 'POST'])
+def ocorrencias():
+    return render_template('nao_disponivel.html', current_time=datetime.utcnow());
+
+
